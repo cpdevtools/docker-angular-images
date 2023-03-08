6d0d079b8eb03fc74c12e8ac9fadf731b37bea9c
Dan Erdelt <erd@conceptual.ca>
2023-03-08 02:50:23 +0000

    ...

--- projects/dom-config-loader/src/index.ts
+++ projects/dom-config-loader/src/index.ts
@@ -1,9 +1,12 @@
 export function loadConfig<TConfig = any>(): TConfig | undefined {
   try {
-    const scriptTag = document.querySelector('> head > script[id="config"][type="application/json"]');
+    const scriptTag = document.querySelector('html > head > script[id="config"][type="application/json"]');
     if (scriptTag?.textContent) {
       return JSON.parse(scriptTag.textContent) as TConfig;
     }
-  } catch {}
+  } catch (e) {
+    console.warn(`Could not load config`);
+    console.warn(e);
+  }
   return undefined;
 }
