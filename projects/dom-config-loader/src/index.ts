b1ec95c0a7b072fe1924442fa54911144f8e8d7f
Erd <erd@conceptual.ca>
2024-02-09 13:42:13 +0000

    Update version numbers and fix config loader

--- projects/dom-config-loader/src/index.ts
+++ projects/dom-config-loader/src/index.ts
@@ -1,8 +1,10 @@
 export function loadConfig<TConfig = any>(): TConfig | undefined {
   try {
-    const scriptTag = document.querySelector('html > head > script[id="config"][type="application/json"]');
-    if (scriptTag?.textContent) {
-      return JSON.parse(scriptTag.textContent) as TConfig;
+    if (typeof document !== "undefined") {
+      const scriptTag = document.querySelector('html > head > script[id="config"][type="application/json"]');
+      if (scriptTag?.textContent) {
+        return JSON.parse(scriptTag.textContent) as TConfig;
+      }
     }
   } catch (e) {
     console.warn(`Could not load config`);
